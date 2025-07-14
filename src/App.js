3b9a85ddec1542da3ae8d2336d5e5bd062049ec0
salomeTIC <joheflopez1@gmail.com>
2025-07-14 16:07:06 -0500

    mi primer cambio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Saludo from './Saludo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>¡Hola React!</h1>
+      <p>Este es mi primer componente.</p>
+      <Saludo nombre="Santiago" />
+      <Saludo nombre="Laura" />
     </div>
   );
 }
 
 export default App;
+
